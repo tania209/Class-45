e2608d887f1def169a16f67da893191ef49441a3
tania209 <70532882+tania209@users.noreply.github.com>
2020-12-22 12:56:56 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+var number;
+var score=0;
+var gift;
+function preload(){
+  chocolateImage = loadImage("chocolate.png")
+  gift1Image = loadImage("gift1.png")
+  gift2Image = loadImage("gift2.png")
+  gift3Image = loadImage("gift3.png")
+  icecreamImage = loadImage("ice cream.png")
+  santaImage = loadImage("santa.png")
+  grinchImage = loadImage("the grinch.png")
+
+}
+function setup(){
+  createCanvas(600,600)
+  santa = createSprite(300,300,40,40)
+  santa.addImage(santaImage)
+  santa.scale = 0.3
+  
+  giftGroup = createGroup();
+
+
+
+  
+
+}
+function draw(){
+  background("white");
+  if(keyDown(UP_ARROW)){
+    santa.y = santa.y -2;
+  }
+  if(keyDown(DOWN_ARROW)){
+    santa.y = santa.y +2
+  }
+  if(keyDown(LEFT_ARROW)){
+    santa.x = santa.x -2
+  }
+  if(keyDown(RIGHT_ARROW)){
+    santa.x = santa.x +2
+  }
+  spawnGift();
+  if(giftGroup.isTouching(santa)){
+    giftGroup.destroyEach();
+    score = score +5;
+
+  }
+  drawSprites();
+  textSize(30)
+  text("score:"+score,50,50)
+  
+}
+function spawnGift(){
+  if(frameCount%100 === 0){
+    gift = createSprite(Math.round(random(50,550)),Math.round(random(50,550)))
+    number = Math.round(random(1,3))
+    if(number === 1){
+      gift.addImage(gift1Image)
+      gift.scale = 0.2
+    }
+    if(number === 2){
+      gift.addImage(gift2Image)
+      gift.scale = 0.1
+    }
+  
+  if(number === 3){
+    gift.addImage(gift3Image)
+    gift.scale = 0.1
+  }
+  giftGroup.add(gift)
+}
+}
